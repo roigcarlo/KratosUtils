4d58a7514358e98f5d2bbe878d5902f57b820e87
roigcarlo <roigcarlo@gmail.com>
2015-11-10 11:56:05 +0100

    Cleanup

--- define.hpp
+++ define.hpp
@@ -1,7 +1,9 @@
 // This file defines things
 #ifndef KRATOS_DEFINES
 #define KRATOS_DEFINES
+
 #define KRATOS_DEFINE_VARIABLE(type, name)      extern type name;
 #define KRATOS_CREATE_VARIABLE(type, name, val) type name = val;
 #define KRATOS_REGISTER_VARIABLE(var) // NOT USED HERE
+
 #endif
