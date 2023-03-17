c95947b2d0ac949201da39628c4b03665f7e0a69
Carlos Roig <roigcarlo@gmail.com>
2023-03-17 13:00:35 +0100

    comments

--- Snippets/Accessor/accessor.cpp
+++ Snippets/Accessor/accessor.cpp
@@ -5,17 +5,21 @@
 class Properties {
 
     // Using the assumtion that this won't change between accessors (as in the discussion)
-    typedef std::function<double(std::string, Properties *)> AccessorType;
+    typedef std::function<double(std::string, Properties *, const double)> AccessorType;
 
     public:
         Properties() {}
         ~Properties() {}
 
+        // I assume that with the trick of using "this" inside the properties caller
+        // we no longer use the "const double" from the accessor function.
+        // If that is true, this template is useless, but I have left it to show how to
+        // forwarding args can be done.
         template<typename ...TArgs>
         double Get(std::string name, TArgs... args) {
             auto value = mAccessors.find(name);
             if (value != mAccessors.end()) {
-                return value->second(name, this);
+                return value->second(name, this, args...);
             } else {
                 return mProperties[name];
             }
@@ -38,15 +42,15 @@ class Properties {
 int main() {
     auto my_properties = Properties();
     
-    my_properties.AddAccessor("alpha", [](std::string name, Properties * my_properties) -> double {
+    my_properties.AddAccessor("alpha", [](std::string name, Properties * my_properties, const double Value) -> double {
         return my_properties->mProperties[name] + 0.1;
     });
 
-    my_properties.AddAccessor("gamma", [](std::string name, Properties * my_properties) -> double {
+    my_properties.AddAccessor("gamma", [](std::string name, Properties * my_properties, const double Value) -> double {
         return 4.0;
     });
 
-    std::cout << "Alpha: " << my_properties.Get("alpha") << std::endl;
-    std::cout << "Beta : " << my_properties.Get("beta")  << std::endl;
-    std::cout << "Gamma: " << my_properties.Get("gamma") << std::endl;
+    std::cout << "Alpha: " << my_properties.Get("alpha", 0) << std::endl;
+    std::cout << "Beta : " << my_properties.Get("beta", 0)  << std::endl;
+    std::cout << "Gamma: " << my_properties.Get("gamma", 0) << std::endl;
 }
