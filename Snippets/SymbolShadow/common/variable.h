9148c3874c45e0f74a5e80b617c54c1d916938c9
Carlos Roig <roigcarlo@gmail.com>
2024-07-05 18:02:50 +0200

    Adding ptr to force crash

--- Snippets/SymbolShadow/common/variable.h
+++ Snippets/SymbolShadow/common/variable.h
@@ -1,16 +1,31 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+
+class DynamicLoad {
+  public:
+    DynamicLoad() {
+        std::cout << "DynamicLoad constructor" << std::endl;
+    }
+
+    ~DynamicLoad() {
+        std::cout << "DynamicLoad destructor" << std::endl;
+    }
+};
 
 class Variable {
   public:
     Variable(int id) {
         mId = id;
+        mDynamicLoad = new DynamicLoad();
     }
 
     ~Variable() {
-        std::cout << "Attempting to destroy variable with id: " << mId << std::endl;
+        std::cout << "Attempting to destroy variable with id: " << mId  << " and ptr pointing to " << mDynamicLoad << std::endl;
+        delete mDynamicLoad;
     }
 
     int mId;
+    DynamicLoad * mDynamicLoad;
 };   
