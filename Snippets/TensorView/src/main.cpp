19495474773e64315bb144a83f8778b3797fa6b8
Carlos Roig <roigcarlo@gmail.com>
2025-06-26 11:36:29 +0200

    Commenting main

--- Snippets/TensorView/src/main.cpp
+++ Snippets/TensorView/src/main.cpp
@@ -10,20 +10,27 @@
 
 int main() {
 
+    // This emulates data inside a container
     std::vector<Data::Array1D<double, 3>> myData(1000);
 
+    // This is how we interact with the data container
     auto ctnr_view = myData | std::views::transform([](Data::Array1D<double, 3>& item) -> double& {return item[0];});
 
+    // This is the minimalistic TensorView implementation that we use to collect and store data
     TensorView<int, decltype(ctnr_view)> tensorView{ctnr_view};
 
+    // Collect data
     tensorView.CollectData();
 
+    // Modify the data (and this is exposed to python as well so you can do whatever you want)
     for (auto& item : tensorView.GetData()) {
         item += 1;
     }
 
+    // Store data back to the original container
     tensorView.StoreData();
 
+    // Print the first 10 items to verify
     for (auto& item: myData | std::views::take(10)) {
         std::cout << item[0] << " ";
     }   
